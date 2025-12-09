cfe5ffc9eb9a8894b208c4a274c72e688d80bf10
Ashley-ALT0153 <ashley.duchesne@oclock.school>
2025-12-09 18:05:54 +0100

    fix bug affichage via url

--- src/router.js
+++ src/router.js
@@ -3,19 +3,19 @@ import {Router} from "express";
 const mainRouter = Router();
 
 mainRouter.get("/", (req, res) => {
-    res.sendFile(`./public/index.html`);
+    res.sendFile(`/home/student/Desktop/html/sandbox/Ashley-ALT0153.github.io/index.html`);
 });
 
 mainRouter.get("/presentation", (req, res) =>{
-    res.sendFile(`/home/student/Desktop/html/sandbox/Ashley-ALT0153.github.io/public/presentation.html`);
+    res.sendFile(`/home/student/Desktop/html/sandbox/Ashley-ALT0153.github.io/presentation.html`);
 });
 
 mainRouter.get("/creation", (req, res) =>{
-    res.sendFile(`/home/student/Desktop/html/sandbox/Ashley-ALT0153.github.io/public/creation.html`);
+    res.sendFile(`/home/student/Desktop/html/sandbox/Ashley-ALT0153.github.io/creation.html`);
 });
 
 mainRouter.get("/contact", (req, res) =>{
-    res.sendFile(`/home/student/Desktop/html/sandbox/Ashley-ALT0153.github.io/public/contact.html`);
+    res.sendFile(`/home/student/Desktop/html/sandbox/Ashley-ALT0153.github.io/contact.html`);
 });
 
 export {mainRouter};
